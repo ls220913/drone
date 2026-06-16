1a4ad03c3621247fc0f2c3e76a4d2a966bf3a27f
ls220913 <lisai220913@163.com>
2026-06-16 19:28:35 +0800

    Update system_stm32f10x.h

--- StartUp/system_stm32f10x.h
+++ StartUp/system_stm32f10x.h
@@ -1,13 +1,5 @@
 /**
-  ******************************************************************************
-  * @file    system_stm32f10x.h
-  * @author  MCD Application Team
-  * @version V3.5.0
-  * @date    11-March-2011
-  * @brief   CMSIS Cortex-M3 Device Peripheral Access Layer System Header File.
-  ******************************************************************************
-  * @attention
-  *
+
   * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
   * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
   * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
@@ -95,4 +87,4 @@ extern void SystemCoreClockUpdate(void);
 /**
   * @}
   */  
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
+
