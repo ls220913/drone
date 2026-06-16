d8a1a63557154b527d8cf259680a3be46c5a1c84
ls220913 <lisai220913@163.com>
2026-06-16 19:19:45 +0800

    Update stm32f10x_crc.c

--- Lib/src/stm32f10x_crc.c
+++ Lib/src/stm32f10x_crc.c
@@ -1,23 +1,4 @@
-/**
-  ******************************************************************************
-  * @file    stm32f10x_crc.c
-  * @author  MCD Application Team
-  * @version V3.5.0
-  * @date    11-March-2011
-  * @brief   This file provides all the CRC firmware functions.
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************
-  */
+
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x_crc.h"
@@ -156,5 +137,3 @@ uint8_t CRC_GetIDRegister(void)
 /**
   * @}
   */
-
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
